5adce1ca17b7ac39a0db6df48da4349df68a67c7
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-05-19 19:25:55 +0530

    Initial Commit for file: data.js

---
+++ data.js
@@ -0,0 +1,398 @@
+export const data = {
+    "903b8494-7043-43d7-9beb-e48c939cb5dc": {
+        "id": "903b8494-7043-43d7-9beb-e48c939cb5dc",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": ""
+    },
+    "d184a983-9dbe-4f69-91ba-126dd08a3870": {
+        "id": "d184a983-9dbe-4f69-91ba-126dd08a3870",
+        "parent_id": "903b8494-7043-43d7-9beb-e48c939cb5dc",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "user_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/0.png",
+        "user_name": "well",
+        "user_title": "soldier"
+    },
+    "6ab07315-ac22-4997-8d7c-2c9ed4fd8f8e": {
+        "id": "6ab07315-ac22-4997-8d7c-2c9ed4fd8f8e",
+        "parent_id": "d184a983-9dbe-4f69-91ba-126dd08a3870",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "stat_label": "particular",
+        "stat_value": "key",
+        "section_title": "the"
+    },
+    "803a78d5-6592-4964-a9b4-34b5c2b8db8e": {
+        "id": "803a78d5-6592-4964-a9b4-34b5c2b8db8e",
+        "parent_id": "6ab07315-ac22-4997-8d7c-2c9ed4fd8f8e",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/45.png",
+        "item_title": "all",
+        "item_description": "Information third guess wide several me. Energy"
+    },
+    "823dee88-746d-4413-a8ab-ee6b39234e6b": {
+        "id": "823dee88-746d-4413-a8ab-ee6b39234e6b",
+        "parent_id": "6ab07315-ac22-4997-8d7c-2c9ed4fd8f8e",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/93.png",
+        "item_title": "clear",
+        "item_description": "Life bring building daughter. White purpose computer"
+    },
+    "1e7b82c2-9e99-43f9-82bb-2d557762489b": {
+        "id": "1e7b82c2-9e99-43f9-82bb-2d557762489b",
+        "parent_id": "6ab07315-ac22-4997-8d7c-2c9ed4fd8f8e",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/16.png",
+        "item_title": "always",
+        "item_description": "Far concern coach start. Side effect image war"
+    },
+    "cc106a95-bd82-422e-8f53-5c7bd7d79551": {
+        "id": "cc106a95-bd82-422e-8f53-5c7bd7d79551",
+        "parent_id": "d184a983-9dbe-4f69-91ba-126dd08a3870",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "stat_label": "whose",
+        "stat_value": "former",
+        "section_title": "most"
+    },
+    "b83398c8-6a70-47c9-9802-298eb028e8e0": {
+        "id": "b83398c8-6a70-47c9-9802-298eb028e8e0",
+        "parent_id": "cc106a95-bd82-422e-8f53-5c7bd7d79551",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/135.png",
+        "item_title": "different",
+        "item_description": "Any window at push form sit color."
+    },
+    "ead7e8c6-be2c-4074-9301-7501e80e4ac7": {
+        "id": "ead7e8c6-be2c-4074-9301-7501e80e4ac7",
+        "parent_id": "cc106a95-bd82-422e-8f53-5c7bd7d79551",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/591.png",
+        "item_title": "civil",
+        "item_description": "In draw behavior shoulder"
+    },
+    "e3bdfdf6-d999-4bfc-aa8c-b8d5adb2f203": {
+        "id": "e3bdfdf6-d999-4bfc-aa8c-b8d5adb2f203",
+        "parent_id": "cc106a95-bd82-422e-8f53-5c7bd7d79551",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/452.png",
+        "item_title": "material",
+        "item_description": "Feeling talk toward specific wide fill boy. According red"
+    },
+    "59a8729c-1af1-4ba5-8b07-1b138ec7bd90": {
+        "id": "59a8729c-1af1-4ba5-8b07-1b138ec7bd90",
+        "parent_id": "d184a983-9dbe-4f69-91ba-126dd08a3870",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "stat_label": "than",
+        "stat_value": "information",
+        "section_title": "mother"
+    },
+    "e078bda6-66a7-4f02-b689-43559ccde97a": {
+        "id": "e078bda6-66a7-4f02-b689-43559ccde97a",
+        "parent_id": "59a8729c-1af1-4ba5-8b07-1b138ec7bd90",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/323.png",
+        "item_title": "war",
+        "item_description": "Test take really one"
+    },
+    "8e898cf4-85e9-4845-9ed4-8268dab7a305": {
+        "id": "8e898cf4-85e9-4845-9ed4-8268dab7a305",
+        "parent_id": "59a8729c-1af1-4ba5-8b07-1b138ec7bd90",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/616.png",
+        "item_title": "dog",
+        "item_description": "Fear argue quality star science movement establish. Region"
+    },
+    "b71e5be4-2cfe-4a39-8863-4cba694326e2": {
+        "id": "b71e5be4-2cfe-4a39-8863-4cba694326e2",
+        "parent_id": "59a8729c-1af1-4ba5-8b07-1b138ec7bd90",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/356.png",
+        "item_title": "main",
+        "item_description": "Provide baby eye lawyer fine."
+    },
+    "9755c806-137a-430b-9cad-3f907cebf88d": {
+        "id": "9755c806-137a-430b-9cad-3f907cebf88d",
+        "parent_id": "903b8494-7043-43d7-9beb-e48c939cb5dc",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "user_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/37.png",
+        "user_name": "person",
+        "user_title": "together"
+    },
+    "e66a9bad-3adf-4b46-ac40-d0196325ba61": {
+        "id": "e66a9bad-3adf-4b46-ac40-d0196325ba61",
+        "parent_id": "9755c806-137a-430b-9cad-3f907cebf88d",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "stat_label": "or",
+        "stat_value": "address",
+        "section_title": "trouble"
+    },
+    "390f6791-2626-4cd2-93ac-83bb76b57e59": {
+        "id": "390f6791-2626-4cd2-93ac-83bb76b57e59",
+        "parent_id": "e66a9bad-3adf-4b46-ac40-d0196325ba61",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/182.png",
+        "item_title": "knowledge",
+        "item_description": "Discuss site require PM law source."
+    },
+    "b0a8b5ac-e0ab-45d0-8ae6-9816f2940530": {
+        "id": "b0a8b5ac-e0ab-45d0-8ae6-9816f2940530",
+        "parent_id": "e66a9bad-3adf-4b46-ac40-d0196325ba61",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/699.png",
+        "item_title": "manager",
+        "item_description": "Yard everybody traditional age thousand so. Care role"
+    },
+    "65befea7-1bcc-4f6e-bcc3-0d5b9d3f58ec": {
+        "id": "65befea7-1bcc-4f6e-bcc3-0d5b9d3f58ec",
+        "parent_id": "e66a9bad-3adf-4b46-ac40-d0196325ba61",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/458.png",
+        "item_title": "what",
+        "item_description": "Price decision say take. Road"
+    },
+    "47556294-d779-4cb9-b967-234532af1fd7": {
+        "id": "47556294-d779-4cb9-b967-234532af1fd7",
+        "parent_id": "9755c806-137a-430b-9cad-3f907cebf88d",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "stat_label": "hotel",
+        "stat_value": "tax",
+        "section_title": "total"
+    },
+    "ddedff44-b68f-4923-ae4f-3ec4d95db410": {
+        "id": "ddedff44-b68f-4923-ae4f-3ec4d95db410",
+        "parent_id": "47556294-d779-4cb9-b967-234532af1fd7",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/566.png",
+        "item_title": "seat",
+        "item_description": "Exactly quickly push require"
+    },
+    "f41ec533-4ba6-42d7-9309-ab3f05ccf27a": {
+        "id": "f41ec533-4ba6-42d7-9309-ab3f05ccf27a",
+        "parent_id": "47556294-d779-4cb9-b967-234532af1fd7",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/588.png",
+        "item_title": "talk",
+        "item_description": "Ground beat above general."
+    },
+    "672e0e9d-7a4a-4110-8e4d-580ad12d1d11": {
+        "id": "672e0e9d-7a4a-4110-8e4d-580ad12d1d11",
+        "parent_id": "47556294-d779-4cb9-b967-234532af1fd7",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/486.png",
+        "item_title": "large",
+        "item_description": "College service effort fast"
+    },
+    "09aa9630-2dec-49aa-aca7-6dd571bb38a7": {
+        "id": "09aa9630-2dec-49aa-aca7-6dd571bb38a7",
+        "parent_id": "9755c806-137a-430b-9cad-3f907cebf88d",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "stat_label": "group",
+        "stat_value": "dream",
+        "section_title": "range"
+    },
+    "3a9d69bd-d28e-4abe-be47-91bd6e931616": {
+        "id": "3a9d69bd-d28e-4abe-be47-91bd6e931616",
+        "parent_id": "09aa9630-2dec-49aa-aca7-6dd571bb38a7",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/534.png",
+        "item_title": "price",
+        "item_description": "Skin only relationship. Standard"
+    },
+    "7fdc5c3f-8836-4a9f-be73-573a03c9ecb8": {
+        "id": "7fdc5c3f-8836-4a9f-be73-573a03c9ecb8",
+        "parent_id": "09aa9630-2dec-49aa-aca7-6dd571bb38a7",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/536.png",
+        "item_title": "argue",
+        "item_description": "Community medical bad step Mrs listen. Help"
+    },
+    "d7ab3868-7901-483b-af18-1955a6441172": {
+        "id": "d7ab3868-7901-483b-af18-1955a6441172",
+        "parent_id": "09aa9630-2dec-49aa-aca7-6dd571bb38a7",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/456.png",
+        "item_title": "station",
+        "item_description": "Late reason identify out. Least remember politics"
+    },
+    "e2f31062-0719-4888-adc9-725f7f2e9715": {
+        "id": "e2f31062-0719-4888-adc9-725f7f2e9715",
+        "parent_id": "903b8494-7043-43d7-9beb-e48c939cb5dc",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "user_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/107.png",
+        "user_name": "help",
+        "user_title": "nation"
+    },
+    "840d94cd-050b-4a7a-bd71-523214e51cd2": {
+        "id": "840d94cd-050b-4a7a-bd71-523214e51cd2",
+        "parent_id": "e2f31062-0719-4888-adc9-725f7f2e9715",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "stat_label": "parent",
+        "stat_value": "can",
+        "section_title": "natural"
+    },
+    "db106954-1aac-4e06-9df8-06f7bdb1957d": {
+        "id": "db106954-1aac-4e06-9df8-06f7bdb1957d",
+        "parent_id": "840d94cd-050b-4a7a-bd71-523214e51cd2",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/613.png",
+        "item_title": "hear",
+        "item_description": "Read daughter care he leader. Rate hour letter"
+    },
+    "2421055c-f5b0-4311-a1c9-26558b9fcfe3": {
+        "id": "2421055c-f5b0-4311-a1c9-26558b9fcfe3",
+        "parent_id": "840d94cd-050b-4a7a-bd71-523214e51cd2",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/725.png",
+        "item_title": "return",
+        "item_description": "Stage street those country behind forget similar. Charge city"
+    },
+    "4ddca8a2-2149-49ce-aa3c-637536892f53": {
+        "id": "4ddca8a2-2149-49ce-aa3c-637536892f53",
+        "parent_id": "840d94cd-050b-4a7a-bd71-523214e51cd2",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/213.png",
+        "item_title": "protect",
+        "item_description": "Expect stop continue"
+    },
+    "1b59c5c8-fae0-4961-98dd-907c1279eb49": {
+        "id": "1b59c5c8-fae0-4961-98dd-907c1279eb49",
+        "parent_id": "e2f31062-0719-4888-adc9-725f7f2e9715",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "stat_label": "parent",
+        "stat_value": "each",
+        "section_title": "price"
+    },
+    "50a81a76-4e59-4283-80e7-9754177881a5": {
+        "id": "50a81a76-4e59-4283-80e7-9754177881a5",
+        "parent_id": "1b59c5c8-fae0-4961-98dd-907c1279eb49",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/90.png",
+        "item_title": "last",
+        "item_description": "Continue stand month sense"
+    },
+    "0967fd1a-d641-4b57-a4ca-f3611bfb21f2": {
+        "id": "0967fd1a-d641-4b57-a4ca-f3611bfb21f2",
+        "parent_id": "1b59c5c8-fae0-4961-98dd-907c1279eb49",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/494.png",
+        "item_title": "head",
+        "item_description": "Clear capital option price. Maybe cold civil method."
+    },
+    "d297b3b4-6813-4ac2-b285-945e842715b7": {
+        "id": "d297b3b4-6813-4ac2-b285-945e842715b7",
+        "parent_id": "1b59c5c8-fae0-4961-98dd-907c1279eb49",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/296.png",
+        "item_title": "together",
+        "item_description": "Practice artist color collection Democrat building almost service."
+    },
+    "2032414c-0d1b-45fa-ac1f-f37f9680023b": {
+        "id": "2032414c-0d1b-45fa-ac1f-f37f9680023b",
+        "parent_id": "e2f31062-0719-4888-adc9-725f7f2e9715",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "stat_label": "fear",
+        "stat_value": "situation",
+        "section_title": "myself"
+    },
+    "80e7ba6d-5fc6-4492-bdef-a053207e58a8": {
+        "id": "80e7ba6d-5fc6-4492-bdef-a053207e58a8",
+        "parent_id": "2032414c-0d1b-45fa-ac1f-f37f9680023b",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/566.png",
+        "item_title": "dream",
+        "item_description": "His court exactly. Painting"
+    },
+    "5a8ce33a-d51d-4309-bfb9-3eddf0c54307": {
+        "id": "5a8ce33a-d51d-4309-bfb9-3eddf0c54307",
+        "parent_id": "2032414c-0d1b-45fa-ac1f-f37f9680023b",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/225.png",
+        "item_title": "exist",
+        "item_description": "Structure head win mean gun"
+    },
+    "f0e7fe7e-4745-4566-b9ab-c718b4a58a20": {
+        "id": "f0e7fe7e-4745-4566-b9ab-c718b4a58a20",
+        "parent_id": "2032414c-0d1b-45fa-ac1f-f37f9680023b",
+        "index": 0,
+        "created_at": "05/19/2023 19:22:29",
+        "created_by": "",
+        "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/89.png",
+        "item_title": "stay",
+        "item_description": "Significant painting board whose. Early one TV health"
+    }
+}
