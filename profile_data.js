84d271f6f3df004864a0603623417f7fa91a0a36
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-05-19 19:25:53 +0530

    Initial Commit for file: profile_data.js

---
+++ profile_data.js
@@ -0,0 +1,97 @@
+export const item = {
+    "user_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/0.png",
+    "id": "d184a983-9dbe-4f69-91ba-126dd08a3870",
+    "user_name": "well",
+    "user_title": "soldier",
+    "profile_stats": [
+        {
+            "stat_label": "particular",
+            "id": "6ab07315-ac22-4997-8d7c-2c9ed4fd8f8e",
+            "stat_value": "key"
+        },
+        {
+            "stat_label": "whose",
+            "id": "cc106a95-bd82-422e-8f53-5c7bd7d79551",
+            "stat_value": "former"
+        },
+        {
+            "stat_label": "than",
+            "id": "59a8729c-1af1-4ba5-8b07-1b138ec7bd90",
+            "stat_value": "information"
+        }
+    ],
+    "profile_sections": [
+        {
+            "section_title": "the",
+            "id": "6ab07315-ac22-4997-8d7c-2c9ed4fd8f8e",
+            "section_items": [
+                {
+                    "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/45.png",
+                    "id": "803a78d5-6592-4964-a9b4-34b5c2b8db8e",
+                    "item_title": "all",
+                    "item_description": "Information third guess wide several me. Energy"
+                },
+                {
+                    "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/93.png",
+                    "id": "823dee88-746d-4413-a8ab-ee6b39234e6b",
+                    "item_title": "clear",
+                    "item_description": "Life bring building daughter. White purpose computer"
+                },
+                {
+                    "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/16.png",
+                    "id": "1e7b82c2-9e99-43f9-82bb-2d557762489b",
+                    "item_title": "always",
+                    "item_description": "Far concern coach start. Side effect image war"
+                }
+            ]
+        },
+        {
+            "section_title": "most",
+            "id": "cc106a95-bd82-422e-8f53-5c7bd7d79551",
+            "section_items": [
+                {
+                    "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/135.png",
+                    "id": "b83398c8-6a70-47c9-9802-298eb028e8e0",
+                    "item_title": "different",
+                    "item_description": "Any window at push form sit color."
+                },
+                {
+                    "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/591.png",
+                    "id": "ead7e8c6-be2c-4074-9301-7501e80e4ac7",
+                    "item_title": "civil",
+                    "item_description": "In draw behavior shoulder"
+                },
+                {
+                    "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/452.png",
+                    "id": "e3bdfdf6-d999-4bfc-aa8c-b8d5adb2f203",
+                    "item_title": "material",
+                    "item_description": "Feeling talk toward specific wide fill boy. According red"
+                }
+            ]
+        },
+        {
+            "section_title": "mother",
+            "id": "59a8729c-1af1-4ba5-8b07-1b138ec7bd90",
+            "section_items": [
+                {
+                    "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/323.png",
+                    "id": "e078bda6-66a7-4f02-b689-43559ccde97a",
+                    "item_title": "war",
+                    "item_description": "Test take really one"
+                },
+                {
+                    "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/616.png",
+                    "id": "8e898cf4-85e9-4845-9ed4-8268dab7a305",
+                    "item_title": "dog",
+                    "item_description": "Fear argue quality star science movement establish. Region"
+                },
+                {
+                    "item_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/356.png",
+                    "id": "b71e5be4-2cfe-4a39-8863-4cba694326e2",
+                    "item_title": "main",
+                    "item_description": "Provide baby eye lawyer fine."
+                }
+            ]
+        }
+    ]
+}
